1b80226c2d5e8eb68904e86b5e02dd9e649de917
DaeJayD <144988177+DaeJayD@users.noreply.github.com>
2023-09-14 05:16:01 -0700

    Add files via upload

---
+++ Doños.py
@@ -0,0 +1,16 @@
+#Create a simple Python program that prints the following information:
+
+name = input("Complete Name: ")
+course = input("Course: ")
+section = input("Section: ")
+age = int(input("Age: "))
+birthday = input("Birthday: ")
+complete_home_adress = input("Complete Home Address: ")
+
+print(name)
+print(course)
+print(section)
+print(age)
+print(birthday)
+print(complete_home_adress)
+
